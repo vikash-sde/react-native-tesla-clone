c7826f0d761af568ca87796039e0630c00a15bfc
Vikash Gupta <vikashg2512@gmail.com>
2022-03-20 19:13:34 +0530

    Initial Setup

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer}>
+        <View style={styles.titles}>
+          <Text style={styles.title}> Model S</Text>
+          <Text style={styles.subTitle}> Starting at $69,420</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  carContainer: {},
 });
