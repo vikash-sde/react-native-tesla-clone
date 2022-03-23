9bf3d5d17b268e8fe403717b94d557dea9d45627
Vikash Gupta <vikashg2512@gmail.com>
2022-03-23 16:13:02 +0530

    Update !!

--- components/CarItem/CarItemStyle.js
+++ components/CarItem/CarItemStyle.js
@@ -1,4 +1,4 @@
-import { StyleSheet} from 'react-native'
+import { StyleSheet,Dimensions} from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
   },
   carContainer: {
     width: "100%",
-    height: "100%",
+    height: Dimensions.get('window').height,
   },
   titles: {
     marginTop: "30%",
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   subTitleCTA: {
-   textDecorationLine:'underline',
+    textDecorationLine: "underline",
   },
 });
 
