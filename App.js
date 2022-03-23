0a38396df744411521a3d3e01a1acc22f4a1e372
Vikash Gupta <vikashg2512@gmail.com>
2022-03-23 19:33:24 +0530

    Header and Flatlist setup

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import CarsList from "./components/CarsList/CarsList";
+import Header from "./components/Header/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <CarsList />
     </View>
   );
--- components/CarsList/CarsList.js
+++ components/CarsList/CarsList.js
@@ -1,4 +1,4 @@
-import { FlatList, View } from "react-native";
+import { FlatList, View, Dimensions } from "react-native";
 import CarItem from "../CarItem/CarItem";
 import styles from "./CarsListStyle";
 import cars from "./cars";
@@ -6,7 +6,14 @@ import cars from "./cars";
 const CarsList = () => {
   return (
     <View style={styles.container}>
-      <FlatList data={cars} renderItem={({ item }) => <CarItem car={item} />} />
+      <FlatList
+        data={cars}
+        renderItem={({ item }) => <CarItem car={item} />}
+        snapToAlignment={"start"}
+        decelerationRate={"fast"}
+        snapToInterval={Dimensions.get("window").height}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
